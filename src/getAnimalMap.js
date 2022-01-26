fcf7ac6cac866ef34a4555ed927a05bcdaf1ea0f
Gabriel Alves <gabrielalves@outlook.com>
2022-01-25 22:56:28 -0300

    Implements the "getAnimalMap" function

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,57 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function getAllSpeciesLocations() {
+  const allLocations = data.species.map(({ location }) => location);
+  const uniqueLocations = [...new Set(allLocations)];
+
+  return uniqueLocations;
+}
+
+function getSpeciesFromLocation(location) {
+  return data.species.filter((specie) => specie.location === location);
+}
+
+function getResidentsNamesWithSexFilter(residents, sex) {
+  const animals = sex
+    ? residents.filter((resident) => resident.sex === sex)
+    : residents;
+
+  const names = animals.map((animal) => animal.name);
+
+  return names;
+}
+
+function getSpeciesWithResidentsNames(species, { sorted, sex }) {
+  return species.map((specie) => {
+    const names = getResidentsNamesWithSexFilter(specie.residents, sex);
+
+    if (sorted) names.sort();
+
+    return { [specie.name]: names };
+  });
+}
+
+function getSpeciesNames(species) {
+  return species.map((specie) => specie.name);
+}
+
+function getAnimalsFromLocation(location, options) {
+  const speciesFromLocation = getSpeciesFromLocation(location);
+
+  return options.includeNames
+    ? getSpeciesWithResidentsNames(speciesFromLocation, options)
+    : getSpeciesNames(speciesFromLocation);
+}
+
+function getAnimalMap(options = {}) {
+  const locations = getAllSpeciesLocations();
+  const animalMap = {};
+
+  locations.forEach((location) => {
+    animalMap[location] = getAnimalsFromLocation(location, options);
+  });
+
+  return animalMap;
 }
 
 module.exports = getAnimalMap;
