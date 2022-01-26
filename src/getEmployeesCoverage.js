b438f51fd21aecb8318ef50ec0325bd71fb5cbd6
Gabriel Alves <gabrielalves@outlook.com>
2022-01-26 11:51:34 -0300

    Implements the "getEmployeesCoverage" function

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,48 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
+const getSpeciesByIds = require('./getSpeciesByIds');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployeeById(id) {
+  return employees.find((employee) => employee.id === id);
+}
+
+function getEmployeeByFirstOrLastName(name) {
+  return employees.find((e) => e.firstName === name || e.lastName === name);
+}
+
+function getSpeciesLocations(species) {
+  return species.map((specie) => specie.location);
+}
+
+function getSpeciesNames(species) {
+  return species.map((specie) => specie.name);
+}
+
+function getEmployeeCoverage(employee) {
+  const species = getSpeciesByIds(...employee.responsibleFor);
+  const locations = getSpeciesLocations(species);
+
+  return {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: getSpeciesNames(species),
+    locations,
+  };
+}
+
+function getEmployeesCoverage(options) {
+  if (options === undefined) {
+    return employees.map((employee) => getEmployeeCoverage(employee));
+  }
+
+  const employee = options.id
+    ? getEmployeeById(options.id)
+    : getEmployeeByFirstOrLastName(options.name);
+
+  if (employee === undefined) {
+    throw new Error('Informações inválidas');
+  }
+
+  return getEmployeeCoverage(employee);
 }
 
 module.exports = getEmployeesCoverage;
