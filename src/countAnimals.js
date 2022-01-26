7c4b869e53e1549e475b3ed2c45b2f76418963b1
Gabriel Alves <gabrielalves@outlook.com>
2022-01-25 22:48:56 -0300

    Implements the "countAnimals" function

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,27 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+const getSpecieByName = require('./getSpecieByName');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAllAnimals() {
+  const animals = {};
+
+  species.forEach((specie) => {
+    animals[specie.name] = specie.residents.length;
+  });
+
+  return animals;
+}
+
+function countAnimals(options) {
+  if (options === undefined) {
+    return countAllAnimals();
+  }
+
+  const specie = getSpecieByName(options.specie);
+  const residents = options.sex
+    ? specie.residents.filter((resident) => resident.sex === options.sex)
+    : specie.residents;
+
+  return residents.length;
 }
 
 module.exports = countAnimals;
