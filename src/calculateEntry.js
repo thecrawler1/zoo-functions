f4dbf9328734cddc6e6e52f82a5c0044fe621486
Gabriel Alves <gabrielalves@outlook.com>
2022-01-25 22:51:20 -0300

    Implements the "calculateEntry" function

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,36 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const children = entrants.filter((e) => e.age < 18);
+  const adults = entrants.filter((e) => e.age >= 18 && e.age < 50);
+  const seniors = entrants.filter((e) => e.age >= 50);
+
+  return {
+    child: children.length,
+    adult: adults.length,
+    senior: seniors.length,
+  };
+}
+
+function isUndefinedOrEmpty(x) {
+  return x === undefined || Object.keys(x).length === 0;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (isUndefinedOrEmpty(entrants)) {
+    return 0;
+  }
+
+  const counts = countEntrants(entrants);
+  let total = 0;
+
+  Object
+    .keys(counts)
+    .forEach((category) => {
+      total += prices[category] * counts[category];
+    });
+
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
