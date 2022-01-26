98af7b25f6305ae74a4558ff4f2dbd6a18852095
Gabriel Alves <gabrielalves@outlook.com>
2022-01-26 09:25:10 -0300

    Implements the "getOldestFromFirstSpecies" function

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,27 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+function getEmployeeById(id) {
+  return employees.find((employee) => employee.id === id);
+}
+
+function getSpecieById(id) {
+  return species.find((specie) => specie.id === id);
+}
+
+function getOldestAnimalFromSpecie(specie) {
+  const copyOfResidents = [...specie.residents];
+  copyOfResidents.sort((a, b) => b.age - a.age);
+
+  return copyOfResidents[0];
+}
+
+function getOldestFromFirstSpecies(employeeId) {
+  const employee = getEmployeeById(employeeId);
+  const firstSpecieId = employee.responsibleFor[0];
+  const firstSpecie = getSpecieById(firstSpecieId);
+  const oldestAnimal = getOldestAnimalFromSpecie(firstSpecie);
+
+  return Object.values(oldestAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
